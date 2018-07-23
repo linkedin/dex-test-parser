092eb96023c0c342d2b2c349ee709558547fd85e
Kyle Koser <kkoser@linkedin.com>
2018-07-23 13:21:47 -0700

    Add support for test classes that extend other test classes - #13 (#16)
    * Add support for test classes that extend other test classes
    
    This supports both abstract and concrete superclasses.
    
    Normally, test methods are found via reflection which is why methods defined from the super class would be included. This
    adds the same behavior to dex-test-parser to match with what is expected when running tests via adb directly.
    
    * Fix test to include the new abstract test methods in expectation
    
    * Fix naming, remove superfluous logs
    
    * Bump version to 2.0
    
    This is a breaking change to the methods to find JUnit4 methods. Also updated the changelog to indicate this
    
    * Update changelog styling
    
    We want to have a section for the current snapshot, so people can add changes there as they are added

--- parser/src/main/kotlin/com/linkedin/dex/parser/JUnit4Extensions.kt
+++ parser/src/main/kotlin/com/linkedin/dex/parser/JUnit4Extensions.kt
@@ -5,22 +5,55 @@
 package com.linkedin.dex.parser
 
 import com.linkedin.dex.spec.ACC_INTERFACE
+import com.linkedin.dex.spec.ACC_ABSTRACT
 import com.linkedin.dex.spec.AnnotationsDirectoryItem
 import com.linkedin.dex.spec.ClassDefItem
 import com.linkedin.dex.spec.DexFile
 import com.linkedin.dex.spec.MethodIdItem
 
 /**
- * Find all methods that are annotated with JUnit4's @Test annotation
+ * Find all methods that are annotated with JUnit4's @Test annotation, including any test methods that
+ * may be inherited from superclasses or interfaces.
  */
-fun DexFile.findJUnit4Tests(): List<TestMethod> {
+fun findAllJUnit4Tests(dexFiles: List<DexFile>): List<TestMethod> {
     val testAnnotationName = "org.junit.Test"
-    val classesWithAnnotations = classDefs.filter(::hasAnnotations).filterNot(::isInterface)
 
-    return createTestMethods(classesWithAnnotations, findMethodIds())
-            .filter { it.annotations.map {
+    // Map to hold all the class information we've found as we go
+    // From the docs:
+    // The classes must be ordered such that a given class's superclass and
+    // implemented interfaces appear in the list earlier than the referring class
+    val classTestMethods: MutableMap<String, ClassParsingResult> = mutableMapOf()
+
+    dexFiles.map {dexFile ->
+
+        // We include classes that do not have annotations because there may be an intermediary class without tests
+        // For example, TestClass1 defines a test, EmptyClass2 extends TestClass1 and defines nothing, and then TestClass2
+        // extends EmptyClass2, TestClass2 should also list the tests defined in TestClass1
+        val classesWithAnnotations = dexFile.classDefs.filterNot(::isInterface)
+
+        classesWithAnnotations.map { classDef ->
+            val baseTests = dexFile.createTestMethods(classDef, dexFile.findMethodIds()).filter { it.annotations.map {
                 it.name
             }.contains(testAnnotationName) }
+
+            val superTests = createTestMethodsFromSuperMethods(dexFile.formatClassName(classDef), getSuperTestMethods(classDef, classTestMethods, dexFile))
+            classTestMethods[dexFile.getClassName(classDef)] = ClassParsingResult(dexFile.getSuperclassName(classDef), baseTests union superTests, !(isAbstract(classDef) || isInterface(classDef)))
+        }
+    }
+
+    return classTestMethods.values.filter { it.isConcrete }.flatMap { it.testMethods }.toList()
+}
+
+/**
+ * Gets the superclass' test methods, so they can be transferred into the subclass as well
+ *
+ * Because we build the parsed classes map with the full list of test methods for a given class (including super methods),
+ * we don't need to actually traverse up the tree here. The immediate superclass will contain all other methods in it
+ * already
+ */
+private fun getSuperTestMethods(classDefItem: ClassDefItem, classTestMethods: Map<String, ClassParsingResult>, dexFile: DexFile): Set<TestMethod> {
+    val superClass = dexFile.getSuperclassName(classDefItem)
+    return classTestMethods[superClass]?.testMethods ?: emptySet()
 }
 
 /**
@@ -30,6 +63,35 @@ private fun DexFile.findMethodIds(): (ClassDefItem, AnnotationsDirectoryItem?) -
     return { _, directory -> directory?.methodAnnotations?.map { methodIds[it.methodIdx] } ?: emptyList() }
 }
 
+private fun DexFile.getClassName(classDefItem: ClassDefItem): String {
+    return ParseUtils.parseClassName(byteBuffer, classDefItem, typeIds, stringIds)
+}
+
+private fun DexFile.getSuperclassName(classDefItem: ClassDefItem): String {
+    val superClassIdx = classDefItem.superclassIdx
+    val typeId = typeIds[superClassIdx]
+
+    return ParseUtils.parseDescriptor(byteBuffer, typeId, stringIds)
+}
+
+/**
+ * Creates new TestMethod objects with the class name changed from the super class to the subclass
+ */
+private fun createTestMethodsFromSuperMethods(className: String, superTests: Set<TestMethod>): Set<TestMethod> {
+    return superTests.map {
+        TestMethod(className + (it.testName.substring(it.testName.indexOf('#') + 1)), it.annotations)
+    }.toSet()
+}
+
 private fun isInterface(classDefItem: ClassDefItem): Boolean {
     return classDefItem.accessFlags and ACC_INTERFACE == ACC_INTERFACE
 }
+
+private fun isAbstract(classDefItem: ClassDefItem): Boolean {
+    return classDefItem.accessFlags and ACC_ABSTRACT == ACC_ABSTRACT
+}
+
+// Class to hold the information we have parsed about the classes we have already seen
+// We need to hold the information for every class, since there is no way to know if a later class will subclass it or not
+// We keep isConcrete as well to make filtering at the end easier
+private data class ClassParsingResult(val superClassName: String, val testMethods: Set<TestMethod>, val isConcrete: Boolean)
---
+++ test-app/src/androidTest/java/com/linkedin/parser/test/junit4/java/ConcreteTest.java
@@ -0,0 +1,12 @@
+package com.linkedin.parser.test.junit4.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConcreteTest extends AbstractTest {
+    @Test
+    public void concreteTest() {
+        assertTrue(true);
+    }
+}
